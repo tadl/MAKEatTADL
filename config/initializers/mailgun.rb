07231ede1748b23b8a7116ad0ee67e3728af7680
William Rockwood <wrockwood@tadl.org>
2025-07-20 09:20:34 -0400

    guard mailgun initializer to prevent explosion during precompile
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,7 +1,11 @@
 # config/initializers/mailgun.rb
 require "mailgun_rails"
 
-Mailgun.configure do |config|
-  config.api_key = ENV.fetch("MAILGUN_API_KEY")
-  config.domain  = ENV.fetch("MAILGUN_DOMAIN")
+if ENV["MAILGUN_API_KEY"].present? && ENV["MAILGUN_DOMAIN"].present?
+  Mailgun.configure do |config|
+    config.api_key = ENV.fetch("MAILGUN_API_KEY")
+    config.domain  = ENV.fetch("MAILGUN_DOMAIN")
+  end
+else
+  Rails.logger.warn "MAILGUN_API_KEY or MAILGUN_DOMAIN not set; skipping Mailgun initialization."
 end
