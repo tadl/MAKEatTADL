529b28168b54af88d4aa99f10aff9fe389146bd4
William Rockwood <wrockwood@tadl.org>
2025-07-20 09:47:13 -0400

    fix another migration issue
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- db/migrate/20250711143605_add_admin_to_staff_users.rb
+++ db/migrate/20250711143605_add_admin_to_staff_users.rb
@@ -1,5 +1,11 @@
 class AddAdminToStaffUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :staff_users, :admin, :boolean, default: false, null: false
+    # only add the column if it doesn’t already exist
+    add_column :staff_users,
+               :admin,
+               :boolean,
+               default: false,
+               null:   false,
+               if_not_exists: true
   end
 end
