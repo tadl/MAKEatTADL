4afd6b8ff65018699082d6788c135d8df4c88a5e
William Rockwood <wrockwood@tadl.org>
2025-07-20 09:53:01 -0400

    another backfill issue. note to self: do not use migrations to backfill
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- db/migrate/20250714203631_fix_category_on_jobs.rb
+++ db/migrate/20250714203631_fix_category_on_jobs.rb
@@ -1,24 +1,7 @@
+# db/migrate/20250714203631_fix_category_on_jobs.rb
 class FixCategoryOnJobs < ActiveRecord::Migration[7.1]
-  disable_ddl_transaction!
-
-  def up
-    change_column_null :jobs, :category_id, true
-
-    say_with_time "Backfilling jobs.category_id from default 'Patron'" do
-      default = Category.find_by!(name: 'Patron').id
-      Job.where(category_id: nil).update_all(category_id: default)
-    end
-
+  def change
+    # Once every job already has a category_id, enforce NOT NULL
     change_column_null :jobs, :category_id, false
-
-    # only add the FK if it isn't already there
-    unless foreign_key_exists?(:jobs, :categories)
-      add_foreign_key :jobs, :categories, validate: true
-    end
-  end
-
-  def down
-    remove_foreign_key :jobs, :categories if foreign_key_exists?(:jobs, :categories)
-    change_column_null :jobs, :category_id, true
   end
 end
