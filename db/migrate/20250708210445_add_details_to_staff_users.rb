aa96086c6938ea8c53001e23f4da19776e058fcb
William Rockwood <wrockwood@tadl.org>
2025-07-20 09:45:21 -0400

    fix another staff_users migration
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- db/migrate/20250708210445_add_details_to_staff_users.rb
+++ db/migrate/20250708210445_add_details_to_staff_users.rb
@@ -1,10 +1,10 @@
 # db/migrate/20250708_add_details_to_staff_users.rb
 class AddDetailsToStaffUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :staff_users, :name,       :string
-    add_column :staff_users, :avatar_url, :string
-    # If you haven’t already, add uid for OmniAuth:
-    add_column :staff_users, :uid,        :string, null: false
-    add_index  :staff_users, :uid, unique: true
+    # only add these columns if they’re not already present
+    add_column :staff_users, :name,       :string,  if_not_exists: true
+    add_column :staff_users, :avatar_url, :string,  if_not_exists: true
+    add_column :staff_users, :uid,        :string,  null: false, if_not_exists: true
+    add_column :staff_users, :admin,      :boolean, null: false, default: false, if_not_exists: true
   end
 end
