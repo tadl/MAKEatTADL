515a33c636d07b150ba14c5ab186a620e14f5f6a
William Rockwood <wrockwood@tadl.org>
2025-07-20 12:01:13 -0400

    tweak prod env
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.assume_ssl = true
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
