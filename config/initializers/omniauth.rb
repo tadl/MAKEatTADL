2e7c61bc72a4e0b1b0f3230add17778fafc53500
William Rockwood <wrockwood@tadl.org>
2025-07-21 12:58:40 -0400

    pay attention to x forwarded host/proto in production
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,18 +1,28 @@
 # config/initializers/omniauth.rb
 
-# Always allow both GET and POST for callbacks
+# allow both GET and POST
 OmniAuth.config.allowed_request_methods = [:post, :get]
 
-# Only mount the Google OAuth2 middleware if credentials are present
+if Rails.env.production?
+  # build the callback URL from X-Forwarded headers
+  OmniAuth.config.full_host = lambda do |env|
+    scheme = env['HTTP_X_FORWARDED_PROTO'] || env['rack.url_scheme']
+    host   = env['HTTP_X_FORWARDED_HOST'] || env['HTTP_HOST']
+    "#{scheme}://#{host}"
+  end
+end
+
 if ENV['GOOGLE_CLIENT_ID'].present? && ENV['GOOGLE_CLIENT_SECRET'].present?
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2,
       ENV['GOOGLE_CLIENT_ID'],
       ENV['GOOGLE_CLIENT_SECRET'],
       {
-        hd:     ENV['GOOGLE_DOMAIN'],   # optional, can be nil
+        hd:     ENV['GOOGLE_DOMAIN'],   # optional
         scope:  'email,profile',
         prompt: 'select_account'
+        # you can also hard-code the callback_url if you prefer:
+        # callback_url: "#{ENV['APP_PROTOCOL']}://#{ENV['APP_HOST']}/auth/google_oauth2/callback"
       }
   end
 else
