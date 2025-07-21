fb758414b5026c8f0aad58f6e6bdd9d50267a55b
William Rockwood <wrockwood@tadl.org>
2025-07-21 13:26:29 -0400

    one more
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,11 +50,6 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-  config.ssl_options = {
-    redirect: true,
-    secure_cookies: true,
-    hsts: { subdomains: true }
-  }
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
