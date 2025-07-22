c68f47d1eab45404c2ddfdef8d55d3f191107acb
William Rockwood <wrockwood@tadl.org>
2025-07-22 16:09:28 -0400

    tell audited to ignore token_sent_at
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/initializers/audited.rb
+++ config/initializers/audited.rb
@@ -1,3 +1,9 @@
 # config/initializers/audited.rb
-# point Audited at your StaffUser helper
+
+# Tell Audited how to find the “current user” for its `user` association
 Audited.current_user_method = :current_staff_user
+
+# Skip any attributes that can’t be safely YAML-dumped
+Audited.config do |config|
+  config.ignored_attributes += ['token_sent_at']
+end
