5a4f0f3ad89bcf84bad19e029bbae4c9e6555cf9
William Rockwood <wrockwood@tadl.org>
2025-09-17 16:27:16 -0400

    fix rails admin file attachment issue
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- lib/rails_admin/config/actions/files.rb
+++ lib/rails_admin/config/actions/files.rb
@@ -13,28 +13,62 @@ module RailsAdmin
           bindings[:abstract_model].model == ::Job && authorized?
         end
 
+        register_instance_option :http_methods do
+          [:get, :post, :delete]
+        end
+
+        register_instance_option :pjax? do
+          false
+        end
+
+        register_instance_option :link_icon do
+          'fa fa-file'
+        end
+
         register_instance_option :controller do
           proc do
             @job = @object
 
-            model_key = @job.class.model_name.param_key
-            uploaded_files = params.dig(model_key, :model_files) || []
+            # Collect files from either print_job[...] or job[...], reject blanks like ""
+            model_key      = @job.model_name.param_key.to_sym
+            candidate_sets = [
+              Array(params.dig(model_key, :model_files)),
+              Array(params.dig(:job,     :model_files))
+            ]
+            uploaded_files = candidate_sets.flatten.compact
 
-            # Only attach if at least one file is a real upload object (not an empty string)
-            files_to_attach = uploaded_files.select { |f| f.is_a?(ActionDispatch::Http::UploadedFile) && f.size > 0 }
+            files_to_attach = uploaded_files.select do |f|
+              # Be robust: accept any upload object that quacks like an upload and has bytes
+              f.respond_to?(:original_filename) &&
+                f.respond_to?(:tempfile) &&
+                f.tempfile &&
+                f.tempfile.respond_to?(:size) &&
+                f.tempfile.size.to_i > 0
+            end
 
             if request.post?
+              Rails.logger.debug do
+                names = files_to_attach.map { |f| f.original_filename }.inspect
+                "RailsAdmin Files: attaching #{files_to_attach.size} file(s) to Job##{@job.id}: #{names}"
+              end
+
               if files_to_attach.any?
                 begin
                   @job.model_files.attach(files_to_attach)
-                  flash[:success] = "#{files_to_attach.size} file(s) attached."
+                  # Ensure in-memory instance reflects the new attachments (not strictly required)
+                  @job.reload
+                  flash[:success] = "#{files_to_attach.size} file#{'s' unless files_to_attach.size == 1} attached."
                 rescue => e
+                  Rails.logger.error("RailsAdmin Files attach error: #{e.class}: #{e.message}")
                   flash[:error] = "Attachment failed: #{e.message}"
                 end
               else
-                flash[:error] = "No valid files to attach! (Debug: #{uploaded_files.inspect})"
+                # Helpful debug shows what we actually received
+                debug_preview = uploaded_files.map { |x| x.is_a?(String) ? x.inspect : x.class.name }.inspect
+                flash[:error] = "No valid files to attach. (Received: #{debug_preview})"
               end
-              redirect_to rails_admin.files_path(model_name: 'job', id: @job.id)
+
+              redirect_to rails_admin.files_path(model_name: @abstract_model.to_param, id: @job.id)
               next
             end
 
@@ -46,22 +80,10 @@ module RailsAdmin
               else
                 flash[:error] = "File not found."
               end
-              redirect_to rails_admin.files_path(model_name: 'job', id: @job.id)
+              redirect_to rails_admin.files_path(model_name: @abstract_model.to_param, id: @job.id)
               next
             end
           end
-        end
-
-        register_instance_option :link_icon do
-          'fa fa-file' # fontawesome icon, change if you want
-        end
-
-        register_instance_option :http_methods do
-          [:get, :post, :delete]
-        end
-
-        register_instance_option :pjax? do
-          false
         end
       end
     end
